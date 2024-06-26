5af98287153a38742b4809fa2f75d9fb86569714
Cyberrain94 <bourdierkelvin@gmail.com>
2024-06-26 18:17:32 -0400

    refactor-code / remove images from links
    Co-authored-by: Scott Tompkins <scottctompkins@users.noreply.github.com>

--- Personal-Link-tree-typescript/next.config.js
+++ Personal-Link-tree-typescript/next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['source.unsplash.com'],
-    unoptimized: true,
-  },
-  experimental: {
-    appDir: true,
-  },
 
-  trailingSlash:true,
-  
   distDir:'build',
 
   output:'export'
